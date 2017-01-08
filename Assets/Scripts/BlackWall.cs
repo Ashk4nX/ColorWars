4497d197cb34ef314856751a9b36e70f5ae151d3
Ashkan Abedi <ashkan.abedi@hotmail.com>
2017-01-08 16:03:37 +0330

    Commit 1/8/2017 _ 4:03PM
    Some Changes.

---
+++ Assets/Scripts/BlackWall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider Target)
+	{
+		if (Target.tag == "Sphere") {
+
+			Time.timeScale = 0.2f;
+		
+		}
+	}
+
+	void OnTriggerExit (){
+	
+		Time.timeScale = 1f;
+
+	}
+}
