bb27973f01bcbdba7cb563ff1efcf3893d9d385a
Ashkan Abedi <ashkan.abedi@hotmail.com>
2017-01-08 15:30:05 +0330

    Commit 1/8/2017 _ 3:29PM
    A cool font added. Main gameplay code is in progress. Some cool features
    added.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	private Text myText;
+	private int ScoreValue;
+
+	// Use this for initialization
+	void Start () {
+
+		myText = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ScoreValue = ((int)(Time.time * 15));
+		myText.text = ScoreValue.ToString();
+
+	}
+
+	public void GetScore ()
+	{
+	
+		ScoreValue += 500;
+	
+	}
+}
--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -5,20 +5,77 @@ using UnityEngine;
 public class Sphere : MonoBehaviour {
 
     private Rigidbody SphereBody;
+    private Renderer SphereRenderer;
+	private Score score;
+
     public float SpeedX;
 
 	// Use this for initialization
 	void Start () {
 
-        SphereBody = gameObject.GetComponent<Rigidbody>();
+        SphereBody = gameObject.GetComponent<Rigidbody> ();
+		SphereRenderer = gameObject.GetComponent<Renderer> ();
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        gameObject.transform.position += new Vector3(SpeedX * Time.deltaTime, 0, 0);
-        SpeedX += 0.5f * Time.deltaTime;
+        SphereMove();
+        ColorChange();
 		
 	}
+
+    void SphereMove() {
+
+        gameObject.transform.position += new Vector3(SpeedX * Time.deltaTime, 0, 0);
+        SpeedX += 0.5f * Time.deltaTime;
+
+    }
+
+    void ColorChange() {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (SphereRenderer.material.color == Color.red)
+            {
+                SphereRenderer.material.color = Color.blue;
+            } else
+            {
+                SphereRenderer.material.color = Color.red;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider Target) {
+
+        if (Target.tag == "BlueWall")
+        {
+            Debug.Log("Sphere Hit the Blue Wall");
+
+            if (SphereRenderer.material.color == Color.blue)
+            {
+                Destroy(Target.gameObject);
+            }
+            else
+            {
+                Debug.Log("GAME OVER!");
+            }
+        }
+
+        if (Target.tag == "RedWall")
+        {
+            Debug.Log("Sphere Hit the Red Wall");
+
+            if (SphereRenderer.material.color == Color.red)
+            {
+                Destroy(Target.gameObject);
+            }
+            else
+            {
+                Debug.Log("GAME OVER!");
+            }
+        }
+
+    }
 }
