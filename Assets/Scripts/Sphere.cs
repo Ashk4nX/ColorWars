8952926f2335dcd6c5b7fe015a29e2cdcc59e21e
Ashkan Abedi <ashkan.abedi@hotmail.com>
2017-01-08 13:43:04 +0330

    Commit 1/8/2017 _ 1:42PM
    Sphere Script Created. Walls added and modified.

---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+
+    private Rigidbody SphereBody;
+    public float SpeedX;
+
+	// Use this for initialization
+	void Start () {
+
+        SphereBody = gameObject.GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.position += new Vector3(SpeedX * Time.deltaTime, 0, 0);
+        SpeedX += 0.5f * Time.deltaTime;
+		
+	}
+}
